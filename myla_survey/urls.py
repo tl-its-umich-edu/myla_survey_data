a75cffcae0ca155f6ac46cf368440dadf5fcdaf5
zqian <zqian@umich.edu>
2021-07-12 17:37:40 -0400

    initial checkin of the django project

---
+++ myla_survey/urls.py
@@ -0,0 +1,32 @@
+"""myla_survey URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from . import views
+
+urlpatterns = [
+    path('', views.get_home_template, name='home'),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+    path('generate_csv/', views.generate_csv, name='generate_csv'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
